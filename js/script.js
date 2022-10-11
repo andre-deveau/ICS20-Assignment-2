1b4f53eeaf476cc87fbc66d3e274f9715b73c0af
andre-deveau <113055933+andre-deveau@users.noreply.github.com>
2022-10-11 14:41:38 -0400

    change script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+ function doMathClicked() {}
